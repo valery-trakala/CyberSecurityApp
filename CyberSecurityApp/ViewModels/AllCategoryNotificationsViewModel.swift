333814063e2575bc6e037dcc587a3bbd143a64af
Valery Trakala <vtrakala@delhaize.be>
2024-04-30 18:53:10 +0200

    linted

--- CyberSecurityApp/ViewModels/AllCategoryNotificationsViewModel.swift
+++ CyberSecurityApp/ViewModels/AllCategoryNotificationsViewModel.swift
@@ -43,46 +43,46 @@ final class AllCategoryNotificationsViewModel: ObservableObject {
                 self?.isNextPageLoading = false
                 self?.pageIndex += 1
             }
-                                          } catch {
-                print(error.localizedDescription)
-            }
-                                          }
-                                          
-                                          private func createNotificationsSectionsFromResponse(response: [CategoryNotificationModelResponse]) -> [NotificationsSectionModel]? {
-                var sections: [NotificationsSectionModel] = []
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    private func createNotificationsSectionsFromResponse(response: [CategoryNotificationModelResponse]) -> [NotificationsSectionModel]? {
+        var sections: [NotificationsSectionModel] = []
+        
+        for (_, notification) in response.enumerated() {
+            guard let sectionDate = dateFormatterHelper.formatDate(from: notification.date, to: "MMM dd, yyyy") else { return nil }
+            
+            if let index = sections.firstIndex(where: { $0.date == sectionDate }) {
+                sections[index].notifications.append(notification)
+            } else {
+                guard let notificationDate = dateFormatterHelper.formatDate(from: notification.date, to: "h:mm:ss a") else { return nil }
+                let formattedNotification = CategoryNotificationModelResponse(
+                    id: notification.id,
+                    categoryId: notification.categoryId,
+                    type: notification.type,
+                    severity: notification.severity,
+                    date: notificationDate)
                 
-                for (_, notification) in response.enumerated() {
-                    guard let sectionDate = dateFormatterHelper.formatDate(from: notification.date, to: "MMM dd, yyyy") else { return nil }
-                    
-                    if let index = sections.firstIndex(where: { $0.date == sectionDate }) {
-                        sections[index].notifications.append(notification)
-                    } else {
-                        guard let notificationDate = dateFormatterHelper.formatDate(from: notification.date, to: "h:mm:ss a") else { return nil }
-                        let formattedNotification = CategoryNotificationModelResponse(
-                            id: notification.id,
-                            categoryId: notification.categoryId,
-                            type: notification.type,
-                            severity: notification.severity,
-                            date: notificationDate)
-                        
-                        sections.append(NotificationsSectionModel(notifications: [formattedNotification], date: sectionDate))
-                    }
-                }
-                
-                return sections
+                sections.append(NotificationsSectionModel(notifications: [formattedNotification], date: sectionDate))
             }
-                                          
-                                          func isLastNotification(_ notification: CategoryNotificationModelResponse) -> Bool {
-                let lastNotification = notificationSections.last!.notifications.last!
-                return lastNotification.id == notification.id
-            }
-                                          
-                                          func isLastSection(_ section: NotificationsSectionModel) -> Bool {
-                let lastSection = notificationSections.last!
-                return lastSection.date == section.date
-            }
-                                          
-                                          func isAllDataLoaded() -> Bool {
-                return loadedNotificationsCount != totalNotificationsCount
-            }
-                                          }
+        }
+        
+        return sections
+    }
+    
+    func isLastNotification(_ notification: CategoryNotificationModelResponse) -> Bool {
+        let lastNotification = notificationSections.last!.notifications.last!
+        return lastNotification.id == notification.id
+    }
+    
+    func isLastSection(_ section: NotificationsSectionModel) -> Bool {
+        let lastSection = notificationSections.last!
+        return lastSection.date == section.date
+    }
+    
+    func isAllDataLoaded() -> Bool {
+        return loadedNotificationsCount != totalNotificationsCount
+    }
+}
